4eaa8ad7c83f8c97f10906a07de1b328665f6cd9
SCARDOZO <sacardoz@uwaterloo.ca>
2022-04-01 03:32:08 -0400

    t

--- kfac_jax/_src/optimizer.py
+++ kfac_jax/_src/optimizer.py
@@ -627,7 +627,7 @@ class Optimizer(utils.WithStagedMethods):
     """Updates the damping parameter."""
     new_loss = self.compute_loss_value(new_func_args)
 
-    if self.value_func_has_state:
+    if self._value_func_has_state:
         new_loss = new_loss[0]
 
     # Sync
